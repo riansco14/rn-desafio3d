17e8d38a64f655859b17f214c67445f2ac715a9a
Rian <rian.azevedo@qintess.com>
2024-03-28 11:52:51 -0300

    change: -Instalação de bibliotecas de navegação, svg, estialização e criação/validação de formularios

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,24 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+    const config = getDefaultConfig(__dirname);
+  
+    const { transformer, resolver } = config;
+  
+    config.transformer = {
+      ...transformer,
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    };
 
-config.resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs')
-config.resolver.assetExts.push('glb', 'gltf', 'png', 'jpg')
-
-module.exports = config;
+    resolver.assetExts.push('glb', 'gltf', 'png', 'jpg')
+    resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs', 'svg')
+    
+    config.resolver = {
+      ...resolver,
+      assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: resolver.sourceExts
+    };
+  
+    return config;
+  })();
